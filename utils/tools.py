347f0426d313acacb1827e50aad855888c79f94a
Harsh Panwar <harshpanwar9524@gmail.com>
2022-07-22 18:19:43 +0100

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -55,15 +55,15 @@ def marginLoss(y_pred, y_true):
     :return: torch.Tensor, scalar
     """
     temperature=0.07
-    device = torch.device("cuda") if projections.is_cuda else torch.device("cpu")
+    device = torch.device("cuda") if y_pred.is_cuda else torch.device("cpu")
 
-    dot_product_tempered = torch.mm(projections, projections.T) / temperature
+    dot_product_tempered = torch.mm(y_pred, y_pred.T) / temperature
     # Minus max for numerical stability with exponential. Same done in cross entropy. Epsilon added to avoid log(0)
     exp_dot_tempered = (
         torch.exp(dot_product_tempered - torch.max(dot_product_tempered, dim=1, keepdim=True)[0]) + 1e-5
     )
 
-    mask_similar_class = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
+    mask_similar_class = (y_true.unsqueeze(1).repeat(1, y_true.shape[0]) == targets).to(device)
     mask_anchor_out = (1 - torch.eye(exp_dot_tempered.shape[0])).to(device)
     mask_combined = mask_similar_class * mask_anchor_out
     cardinality_per_samples = torch.sum(mask_combined, dim=1)
